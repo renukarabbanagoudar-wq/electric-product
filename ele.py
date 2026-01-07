0c3aba8f5e0ed5018311b083204dab1cfc903cf4
Renuka <renuka.rabbanagoudar@gmail.com>
2026-01-07 18:00:05 +0530

    Update ele.py

--- ele.py
+++ ele.py
@@ -1,15 +1,15 @@
-product_name = input("Enter Product Name: ")
-product_id = input("Enter Product ID: ")
-price = float(input("Enter Price of Product: "))
-quantity = int(input("Enter Quantity: "))
+import sys
 
-# Calculation
+product_name = sys.argv[1]
+product_id = sys.argv[2]
+price = float(sys.argv[3])
+quantity = int(sys.argv[4])
+
 total_cost = price * quantity
 
-# Output
-print("\n----- Electric Product Details -----")
-print("Product Name   :", product_name)
-print("Product ID     :", product_id)
-print("Price          :", price)
-print("Quantity       :", quantity)
-print("Total Cost     :", total_cost)
+print("----- Electric Product Details -----")
+print("Product Name :", product_name)
+print("Product ID   :", product_id)
+print("Price        :", price)
+print("Quantity     :", quantity)
+print("Total Cost   :", total_cost)
